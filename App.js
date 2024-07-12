4dc1fa4bbf54d1d8782838cb543d3800908496c0
ibnuputraard <ibnu.putra@nawadata.id>
2024-07-12 17:31:52 +0700

    done tetsing on ios

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import Main from './src';
-import {DefaultTheme, PaperProvider} from 'react-native-paper';
+import {
+  ActivityIndicator,
+  DefaultTheme,
+  PaperProvider,
+} from 'react-native-paper';
 import {Colors} from './src/styles';
 import messaging from '@react-native-firebase/messaging';
-import {PermissionsAndroid, Platform} from 'react-native';
+import {PermissionsAndroid, Platform, StyleSheet, View} from 'react-native';
 import codePush from 'react-native-code-push';
 
 const theme = {
@@ -22,6 +26,8 @@ const options = {
 };
 
 const App = () => {
+  const [isLoading, setIsLoading] = React.useState(false);
+
   async function requestUserPermission() {
     if (Platform.OS == 'ios') {
       const authStatus = await messaging().requestPermission();
@@ -42,13 +48,49 @@ const App = () => {
   // request permission
   React.useEffect(() => {
     requestUserPermission();
+
+    const codePushStatusDidChange = status => {
+      switch (status) {
+        case codePush.SyncStatus.DOWNLOADING_PACKAGE:
+          setIsLoading(true);
+          break;
+        case codePush.SyncStatus.INSTALLING_UPDATE:
+          setIsLoading(false);
+          break;
+        case codePush.SyncStatus.UPDATE_INSTALLED:
+          setIsLoading(false);
+          break;
+        default:
+          break;
+      }
+    };
+
+    const syncWithCodePush = () => {
+      codePush.sync({}, codePushStatusDidChange);
+    };
+
+    syncWithCodePush();
   }, []);
 
   return (
     <PaperProvider theme={theme}>
+      {isLoading && (
+        <View style={styles.loadingContainer}>
+          <ActivityIndicator size="large" color={Colors.COLOR_PRIMARY} />
+        </View>
+      )}
       <Main />
     </PaperProvider>
   );
 };
+
+const styles = StyleSheet.create({
+  loadingContainer: {
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0,0,0,0.5)',
+  },
+});
 
 export default codePush(options)(App);
